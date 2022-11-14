d04706c012d684db95b2bff8f34bc2b3cc92fb00
Tolmachev DY <Tolmachev DY@DESKTOP-5RJHKAS>
2022-11-14 23:47:34 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] strArrey = str.Split();
+            string max = strArrey[0];
+            foreach(string s in strArrey)
+            {
+                if (s.Length > max.Length)
+                {
+                    max = s;
+                }
+            }
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str1 = str.ToLower();
+            string str2 = str1.Replace(" ","");
+            string str3 = "";
+            foreach (char c in str2)
+            {
+                str3 = c + str3;
+            }
+            if (str2==str3)
+            {
+                Console.WriteLine("строка является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("строка не является палиндромом");
+            }           
+            Console.ReadKey();
+        }
+    }
+}
